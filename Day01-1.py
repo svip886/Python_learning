c456b255e75610a67cb9a2c3a5f4277d08ce7e3f
svip886 <76468077+svip886@users.noreply.github.com>
2021-01-18 00:56:14 -0600

    Day01

---
+++ Day01-1.py
@@ -0,0 +1,9 @@
+"""
+第一个Python程序 - Hello，world！
+
+Version:0.1
+Author:Paranoid
+"""
+print("hello world!")
+#你好，世界
+print(123*123)
---
+++ Day01-2.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.pensize(5)
+turtle.pencolor('red')
+
+turtle.forward(200)
+turtle.right(180)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(180)
+turtle.forward(300)
+turtle.right(180)
+turtle.forward(200)
+turtle.right(30+180)
+turtle.forward(200)
+turtle.right(180)
+turtle.forward(200)
+turtle.right(120)
+turtle.forward(200)
+turtle.mainloop()
